308edebd5955a23a97c8710a8b8b063651675973
sameep <sameep0112vani@gmail.com>
2021-04-02 08:01:01 +0530

    Transaction Added

---
+++ frontend/src/Components/Transaction/TransactionList.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Sidebar from '../Common/Sidebar';
+import Header from '../Common/Header';
+import { Button, Card, CardBody, CardFooter, CardHeader } from 'reactstrap';
+
+class Shipping extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="row">
+                    <div style={{ minHeight: "93vh" }} className="col-2.5">
+                        <Sidebar />
+                    </div>
+                    <div className="col row d-flex justify-content-center" style={{maxHeight: '50vh'}}>
+                        <Card className="m-2 col-md-3">
+                            <CardHeader style={{ backgroundColor: 'lightgreen' }}>Shipping Details</CardHeader>
+                            <CardBody>
+                                <p>Order ID</p>
+                                <p>Product Name</p>
+                                <p>Shipping Details</p>
+                            </CardBody>
+                            <CardFooter>
+                                <div className="d-flex justify-content-center">
+                                    <Button color="primary">View Details</Button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                        <Card className="m-2 col-md-3">
+                            <CardHeader style={{ backgroundColor: 'rgb(250, 64, 89)' }}>Shipping Details</CardHeader>
+                            <CardBody>
+                                <p>Order ID</p>
+                                <p>Product Name</p>
+                                <p>Shipping Details</p>
+                            </CardBody>
+                            <CardFooter>
+                                <div className="d-flex justify-content-center">
+                                    <Button color="primary">View Details</Button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                        <Card className="m-2 col-md-3">
+                            <CardHeader style={{ backgroundColor: 'lightgreen' }}>Shipping Details</CardHeader>
+                            <CardBody>
+                                <p>Order ID</p>
+                                <p>Product Name</p>
+                                <p>Shipping Details</p>
+                            </CardBody>
+                            <CardFooter>
+                                <div className="d-flex justify-content-center">
+                                    <Button color="primary">View Details</Button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Shipping;
