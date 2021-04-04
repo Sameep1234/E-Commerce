76e932d9cc1ac7c9f1297d563d8b081525206fa1
sameep <sameep0112vani@gmail.com>
2021-04-04 10:18:58 +0530

    Updated mistakes in database

--- WMS_Queries.sql
+++ WMS_Queries.sql
@@ -3,25 +3,37 @@ create database if not exists wms;
 use wms;
 
 -- CREATE TABLE QUERIES
-create table brand (brandId varchar(10) primary key, brandName varchar(50) not null);
-
-create table specification (specificationId varchar(10) primary key, productId varchar(10) not null, categoryId varchar(10) not null, subCategoryId varchar(10) not null, 
-							brandId varchar(10) not null, model VARCHAR(25), productDescription VARCHAR(200));
-                        
-create table subCategory (subCategoryId varchar(10) primary key, subCategoryName varchar(50) not null);
-
-create table seller(sellerId varchar(10) primary key, firstName varchar(20) not null, middleName varchar(20), lastName varchar(20) not null,
-							shopNumber varchar(10), address1 varchar(500), landmark varchar(20), area varchar(20),
-							city varchar(50), state varchar(50), country varchar(50), postalCode int, 
-                            contactNumber int not null, email varchar(50) not null, gstNumber int not null);
-                        
 create table buyer(buyerId varchar(10) primary key, firstName varchar(20) not null, middleName varchar(20), lastName varchar(20) not null, 
 							appartmentNumber varchar(10), address1 varchar(500), landmark varchar(100), area varchar(20),
                             city varchar(50), state varchar(50), country varchar(50), postalCode int, 
                             contactNumber int not null, email varchar(50) not null);
-                        
+                            
+create table seller(sellerId varchar(10) primary key, firstName varchar(20) not null, middleName varchar(20), lastName varchar(20) not null,
+							shopNumber varchar(10), address1 varchar(500), landmark varchar(20), area varchar(20),
+							city varchar(50), state varchar(50), country varchar(50), postalCode int, 
+                            contactNumber int not null, email varchar(50) not null, gstNumber int not null);
+                            
+create table brand (brandId varchar(10) primary key, brandName varchar(50) not null);
+
 create table category (categoryId varchar(10) primary key, categoryName varchar(50) not null);
 
+create table employee(employeeId varchar(10) primary key,
+							employeeType varchar(50) not null,
+							firstName varchar(20) not null,
+							middleName varchar(20),
+							lastName varchar(20) not null,
+							salary double not null, check (salary > 0),
+							hiringDate date not null);
+
+create table subCategory (subCategoryId varchar(10) primary key, subCategoryName varchar(50) not null,
+							categoryId varchar(10), foreign key(categoryId) references category(categoryId));
+
+create table specification (specificationId varchar(10) primary key,
+							categoryId varchar(10) not null, foreign key(categoryId) references category(categoryId),
+                            subCategoryId varchar(10) not null, foreign key(subCategoryId) references subcategory(subCategoryId),
+							brandId varchar(10) not null, foreign key(brandId) references brand(brandId),
+                            model VARCHAR(25), productDescription VARCHAR(200));
+                            
 create table product (productId varchar(10) primary key, productName VARCHAR(50) not null, categoryId varchar(10) not null, foreign key(categoryId) references category (categoryId), 
 							subCategoryId varchar(10) not null, foreign key(subCategoryId) references subCategory (subCategoryId),
                             brandId varchar(10) not null, foreign key(brandId) references brand (brandId), 
@@ -38,13 +50,7 @@ create table transaction (transactionId varchar(10) primary key, orderId varchar
 create table shipping(shippingId varchar(10) primary key, orderId varchar(10) not null, foreign key(orderId) references orders(orderId), 
 							dispatchDate date not null, arrivalDate date not null);
                             
-create table employee(employeeId varchar(10) primary key,
-							employeeType varchar(50) not null,
-							firstName varchar(20) not null,
-							middleName varchar(20),
-							lastName varchar(20) not null,
-							salary double not null, check (salary > 0),
-							hiringDate date not null);
+
 
 create table cart(buyerId varchar(10) not null, foreign key(buyerId) references buyer(buyerId),
 							productId varchar(10) not null, foreign key(productId) references product(productId),
