b03b4cc2b1ba0828ad244353dad094444ffebd92
sameep <sameep0112vani@gmail.com>
2021-04-14 16:50:30 +0530

    TEST

--- frontend/src/Components/Cart/Cart.js
+++ frontend/src/Components/Cart/Cart.js
@@ -2,8 +2,62 @@ import React, { Component } from 'react';
 import Sidebar from '../Common/Sidebar';
 import Header from '../Common/Header';
 import { Card, CardHeader, CardBody } from 'reactstrap';
+import axios from 'axios';
+
+function ShowCart({ result }) {
+    let display = result.map((cl) => {
+        if (Object.keys(cl).length > 3) {
+            return (
+                <Card className="col-md-8">
+                    <CardHeader className="row">
+                        <div className="col">
+                            <p className="row">ORDER PLACED</p>
+                            <p className="row"> 18 October 2019</p>
+                        </div>
+                        <div className="col">
+                            <p className="row">TOTAL</p>
+                            <p className="row"> Rs 1,199.00</p>
+                        </div>
+                        <div className="col">
+                            <p className="row">SHIP TO</p>
+                            <p className="row">Sameep Vani</p>
+                        </div>
+                    </CardHeader>
+                    <CardBody>
+                        SOME DESCRIPTION
+                    </CardBody>
+                </Card>
+            )
+        }
+
+    });
+
+    return (display);
+}
 
 class Cart extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            result: [],
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/cartList')
+            .then((response) => {
+                if (response.data.status === 1) {
+                    this.setState({
+                        result: response.data.data,
+                    });
+                }
+            })
+            .catch((err) => {
+                alert(err);
+            })
+    }
+
     render() {
         return (
             <div>
@@ -22,114 +76,10 @@ class Cart extends Component {
                                     <option value="2019">2019</option>
                                 </select>
                             </div>
+                            <div style={{ maxHeight: '35vh' }} className="mt-3 d-flex justify-content-center">
+                                <ShowCart result={this.state.result} />
+                            </div>
                         </div>
-
-                        <div style={{ maxHeight: '35vh' }} className="mt-3 d-flex justify-content-center">
-                            <Card className="col-md-8">
-                                <CardHeader className="row">
-                                    <div className="col">
-                                        <p className="row">ORDER PLACED</p>
-                                        <p className="row"> 18 October 2019</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">TOTAL</p>
-                                        <p className="row"> Rs 1,199.00</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">SHIP TO</p>
-                                        <p className="row">Sameep Vani</p>
-                                    </div>
-                                </CardHeader>
-                                <CardBody>
-                                    SOME DESCRIPTION
-                            </CardBody>
-                            </Card>
-                        </div>
-                        <div style={{ maxHeight: '35vh' }} className="mt-3 d-flex justify-content-center">
-                            <Card className="col-md-8">
-                                <CardHeader className="row">
-                                    <div className="col">
-                                        <p className="row">ORDER PLACED</p>
-                                        <p className="row"> 18 October 2019</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">TOTAL</p>
-                                        <p className="row"> Rs 1,199.00</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">SHIP TO</p>
-                                        <p className="row">Sameep Vani</p>
-                                    </div>
-                                </CardHeader>
-                                <CardBody>
-                                    SOME DESCRIPTION
-                            </CardBody>
-                            </Card>
-                        </div>
-                        <div style={{ maxHeight: '35vh' }} className="mt-3 d-flex justify-content-center">
-                            <Card className="col-md-8">
-                                <CardHeader className="row">
-                                    <div className="col">
-                                        <p className="row">ORDER PLACED</p>
-                                        <p className="row"> 18 October 2019</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">TOTAL</p>
-                                        <p className="row"> Rs 1,199.00</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">SHIP TO</p>
-                                        <p className="row">Sameep Vani</p>
-                                    </div>
-                                </CardHeader>
-                                <CardBody>
-                                    SOME DESCRIPTION
-                            </CardBody>
-                            </Card>
-                        </div>
-                        <div style={{ maxHeight: '35vh' }} className="mt-3 d-flex justify-content-center">
-                            <Card className="col-md-8">
-                                <CardHeader className="row">
-                                    <div className="col">
-                                        <p className="row">ORDER PLACED</p>
-                                        <p className="row"> 18 October 2019</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">TOTAL</p>
-                                        <p className="row"> Rs 1,199.00</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">SHIP TO</p>
-                                        <p className="row">Sameep Vani</p>
-                                    </div>
-                                </CardHeader>
-                                <CardBody>
-                                    SOME DESCRIPTION
-                            </CardBody>
-                            </Card>
-                        </div>
-                        <div style={{ maxHeight: '35vh' }} className="mt-3 d-flex justify-content-center">
-                            <Card className="col-md-8">
-                                <CardHeader className="row">
-                                    <div className="col">
-                                        <p className="row">ORDER PLACED</p>
-                                        <p className="row"> 18 October 2019</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">TOTAL</p>
-                                        <p className="row"> Rs 1,199.00</p>
-                                    </div>
-                                    <div className="col">
-                                        <p className="row">SHIP TO</p>
-                                        <p className="row">Sameep Vani</p>
-                                    </div>
-                                </CardHeader>
-                                <CardBody>
-                                    SOME DESCRIPTION
-                            </CardBody>
-                            </Card>
-                        </div>
-
                     </div>
                 </div>
             </div>
